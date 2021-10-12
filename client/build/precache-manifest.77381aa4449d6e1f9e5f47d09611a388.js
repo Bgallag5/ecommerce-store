2eb049ca4d45f2f1e9742c06b2d0422586321574
bengallagher <bgallag5@gmail.com>
2021-10-12 16:23:43 -0400

    remove from cart

---
+++ client/build/precache-manifest.77381aa4449d6e1f9e5f47d09611a388.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e18015837ba9fc9ce18708d5afd3dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc8e09f5d5d05428c378",
+    "url": "/static/css/main.ad1af64d.chunk.css"
+  },
+  {
+    "revision": "c20fe3075a2400775b3f",
+    "url": "/static/js/2.baa6b9f3.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.baa6b9f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8e09f5d5d05428c378",
+    "url": "/static/js/main.fd2eaf5f.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
